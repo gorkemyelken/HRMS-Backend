e7f62d9492415496c87b9164b26a8947b01fa397
Görkem <gorkemyelken@gmail.com>
2021-09-24 01:44:35 +0300

    HRMS Frontend Update

---
+++ hrms frontend/src/layouts/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <Categories />
+                    </Grid.Column>
+                    <Grid.Column width={12}>
+                        <ProductList />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
+    )
+}
